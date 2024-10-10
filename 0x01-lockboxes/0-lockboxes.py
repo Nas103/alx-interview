146c2ab5bac5550455bb7d2054348f491ac90c96
Nas103 <tumelorhulani33@gmail.com>
2024-10-10 19:01:30 +0000

    py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python3
-
+"""
+Module: lockboxes
+This module contains a function that determines if all boxes in a list
+can be unlocked.
+"""
 
 from collections import deque
+
 
 def canUnlockAll(boxes):
     """
     Determines whether all lockboxes in a given list can be unlocked.
 
     Args:
-        boxes: A list of lists, where each inner list represents the keys in a lockbox.
+        boxes (list of list): A list of lists, where each inner list represents
+        the keys in a lockbox.
 
     Returns:
-        True if all lockboxes can be unlocked, False otherwise.
+        bool: True if all lockboxes can be unlocked, False otherwise.
     """
 
-    n = len(boxes)
-    unlocked = [False] * n
+    n = len(boxes)  # Number of lockboxes
+    unlocked = [False] * n  # Track the status of each box (locked/unlocked)
     unlocked[0] = True  # The first box is always unlocked
 
-    queue = deque([0])  # Queue to store unlocked boxes
+    queue = deque([0])  # Queue to store unlocked boxes for BFS traversal
 
     while queue:
-        current_box = queue.popleft()
+        current_box = queue.popleft()  # Take the next unlocked box
 
+        # Iterate over the keys in the current box
         for key in boxes[current_box]:
+            # Unlock the box corresponding to the key, if it's not already unlocked
             if 0 <= key < n and not unlocked[key]:
                 unlocked[key] = True
-                queue.append(key)
+                queue.append(key)  # Add the newly unlocked box to the queue
 
+    # Check if all boxes have been unlocked
     return all(unlocked)
