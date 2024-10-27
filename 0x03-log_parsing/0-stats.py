6c2ee535aa43ff60b0b2958f3cf9af70a0e28843
Nas103 <tumelorhulani33@gmail.com>
2024-10-27 20:36:56 +0000

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,6 @@
 import sys
 import re
 
-
 status_code_count = {
     200: 0,
     301: 0,
@@ -25,25 +24,27 @@ def print_stats():
 line_count = 0
 total_file_size = 0
 
-
 try:
     for line in sys.stdin:
         line_count += 1
 
-        match = re.match(r'\S+ - (.*?) "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)', line)
+        match = re.match(
+            r'\S+ - \[(.*?)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)',
+            line)
         if match:
             status_code = int(match.group(2))
             file_size = int(match.group(3))
 
             total_file_size += file_size
             if status_code in status_code_count:
-
                 status_code_count[status_code] += 1
 
         if line_count % 10 == 0:
             print_stats()
 
-
+except KeyboardInterrupt:
+    print_stats()
+    raise
 except Exception:
     pass
 finally:
