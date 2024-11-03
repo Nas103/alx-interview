273678f7fd685c0204ea84df64ee9629c2c535e7
Nas103 <tumelorhulani33@gmail.com>
2024-11-03 20:47:04 +0000

    pu

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,45 +1,31 @@
 #!/usr/bin/python3
-"""
-Module to validate UTF-8 encoding
-"""
 
 
 def validUTF8(data):
-    """
-    Checks if a given list of integers is a valid UTF-8 encoding.
+    """Check if a given data set represents a valid UTF-8 encoding."""
+    n_bytes = 0
 
-    Parameters:
-    - data: List[int] - List of integers where each integer represents a byte
-
-    Returns:
-    - bool: True if data is a valid UTF-8 encoding, otherwise False.
-    """
-    n_bytes = 0  # Number of continuation bytes expected
-
-    # Masks to check the most significant bits of each byte
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
+    mask1 = 1 << 7
+    mask2 = 1 << 6
 
     for num in data:
-        # Extract only the 8 least significant bits
-        byte = num & 0xFF
+        bin_rep = bin(num).replace('0b', '').rjust(8, '0')
 
         if n_bytes == 0:
-            # Count leading 1's to determine the number of bytes
-            if (byte & mask1) == 0:
-                continue  # 1-byte character
-            elif (byte & (mask1 >> 1)) == mask1:
-                n_bytes = 1  # 2-byte character
-            elif (byte & (mask1 >> 2)) == mask1:
-                n_bytes = 2  # 3-byte character
-            elif (byte & (mask1 >> 3)) == mask1:
-                n_bytes = 3  # 4-byte character
-            else:
-                return False  # Invalid UTF-8 start byte
-        else:
-            # Check continuation byte starts with '10'
-            if not (byte & mask1 and not (byte & mask2)):
+            for bit in bin_rep:
+                if bit == '0':
+                    break
+                n_bytes += 1
+
+            if n_bytes == 0:
+                continue
+
+            if n_bytes == 1 or n_bytes > 4:
                 return False
-            n_bytes -= 1
+        else:
+            if not (num & mask1 and not (num & mask2)):
+                return False
+
+        n_bytes -= 1
 
     return n_bytes == 0
