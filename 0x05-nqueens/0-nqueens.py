5f034a70652cfc68d849800c1b1b21ef1c541c06
Nas103 <tumelorhulani33@gmail.com>
2024-11-07 15:31:58 +0000

    px

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+N Queens problem solver.
+"""
+
+import sys
+
+
+def is_safe(board, row, col):
+    """
+    Check if it's safe to place a queen at board[row][col].
+    """
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+
+def solve_nqueens(board, col):
+    """
+    Solve the N Queens problem using backtracking.
+    """
+    if col >= len(board):
+        print_solution(board)
+        return True
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens(board, col + 1) or res
+            board[i][col] = 0
+    return res
+
+
+def print_solution(board):
+    """
+    Print the board configuration.
+    """
+    solution = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    board = [[0] * n for _ in range(n)]
+    solve_nqueens(board, 0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 0x05-nqueens/README.md
@@ -0,0 +1,11 @@
+# N Queens Problem
+
+This project solves the N Queens problem using a backtracking algorithm in Python.
+
+## Requirements
+- Python 3.4.3
+- PEP 8 style (version 1.7.*)
+
+## Usage
+```bash
+$ ./nqueens.py N
