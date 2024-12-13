5374f63a647032390bfdf29686f34f920a43fa1e
Nas103 <tumelot>
2024-12-13 03:53:20 +0000

    k

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
 
 
-def sieve_of_eratosthenes(n):
-    """Generate a list of prime numbers up to n using Sieve of Eratosthenes."""
-    primes = [True] * (n + 1)
-    p = 2
-    while p * p <= n:
-        if primes[p]:
-            for i in range(p * p, n + 1, p):
-                primes[i] = False
-        p += 1
-    return [p for p in range(2, n + 1) if primes[p]]
-
-
 def isWinner(x, nums):
-    """Determine the winner of the prime game."""
+    """Determine the winner of the Prime Game."""
     if not nums or x < 1:
         return None
 
-    max_num = max(nums)
-    primes = sieve_of_eratosthenes(max_num)
+    # Find the maximum number in nums for sieve optimization
+    max_n = max(nums)
+
+    # Sieve of Eratosthenes to determine primes up to max_n
+    primes = [True] * (max_n + 1)
+    primes[0] = primes[1] = False  # 0 and 1 are not primes
+
+    for i in range(2, int(max_n**0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, max_n + 1, i):
+                primes[j] = False
+
+    # Cumulative prime counts
+    prime_counts = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_counts[i] = prime_counts[i - 1] + (1 if primes[i] else 0)
 
     maria_wins = 0
     ben_wins = 0
 
+    # Simulate each game
     for n in nums:
-        primes_count = sum(1 for p in primes if p <= n)
-
-        if primes_count % 2 == 1:
-            maria_wins += 1
-        else:
+        if prime_counts[n] % 2 == 0:
             ben_wins += 1
+        else:
+            maria_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
