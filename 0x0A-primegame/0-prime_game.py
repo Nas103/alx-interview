95203078bdd69ec4a20f7625a65a8a7e498a254e
Nas103 <tumelot>
2024-12-13 03:41:18 +0000

    p

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,41 +2,20 @@
 
 
 def sieve_of_eratosthenes(n):
-    """Generate a list of prime numbers """
-    is_prime = [True] * (n + 1)
+    """Generate a list of prime numbers up to n using Sieve of Eratosthenes."""
+    primes = [True] * (n + 1)
     p = 2
-    while (p * p <= n):
-        if is_prime[p] is True:
+    while p * p <= n:
+        if primes[p]:
             for i in range(p * p, n + 1, p):
-                is_prime[i] = False
+                primes[i] = False
         p += 1
-    primes = [p for p in range(2, n + 1) if is_prime[p]]
-    return primes
-
-
-def play_game(n, primes):
-    """Simulate the game for a given n and return the winner."""
-    available_numbers = set(range(1, n + 1))
-    turn = 0  # 0 for Maria, 1 for Ben
-
-    while True:
-        prime_selected = False
-        for prime in primes:
-            if prime in available_numbers:
-                prime_selected = True
-                multiples = set(range(prime, n + 1, prime))
-                available_numbers -= multiples
-                break
-        if not prime_selected:
-            break
-        turn = 1 - turn  # Switch turn
-
-    return "Maria" if turn == 1 else "Ben"
+    return [p for p in range(2, n + 1) if primes[p]]
 
 
 def isWinner(x, nums):
-    """Determine the winner of each game an """
-    if not nums or x <= 0:
+    """Determine the winner of the prime game."""
+    if not nums or x < 1:
         return None
 
     max_num = max(nums)
@@ -46,8 +25,9 @@ def isWinner(x, nums):
     ben_wins = 0
 
     for n in nums:
-        winner = play_game(n, primes)
-        if winner == "Maria":
+        primes_count = sum(1 for p in primes if p <= n)
+
+        if primes_count % 2 == 1:
             maria_wins += 1
         else:
             ben_wins += 1
@@ -58,8 +38,3 @@ def isWinner(x, nums):
         return "Ben"
     else:
         return None
-
-
-# Example Usage
-if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
